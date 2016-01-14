d9f8c41a705b651fda22412d6cca1f39d7e50189
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2016-01-14 20:09:28 +0530

    First commit

---
+++ Movie MVC3/Controllers/AboutController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie_MVC3.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+       public ActionResult Index()
+        //public string Index()
+        {
+            ViewBag.Message = "About us page";
+           // return "This is my <b>default</b> action..."; 
+            //System.Diagnostics.Debug.WriteLine(Request.AcceptTypes.Contains("text/html"));
+            return View();
+        }
+       public ActionResult Welcome(string name, int numTimes)
+       {
+           ViewBag.Title = "Welcome Page";
+           ViewBag.Message = "We take immense pleasure in welcoming the user: " + name;
+           ViewBag.NumTimes = numTimes;
+           return View();
+       }
+
+    }
+}
---
+++ Movie MVC3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movie_MVC3.MvcApplication" Language="C#" %>
